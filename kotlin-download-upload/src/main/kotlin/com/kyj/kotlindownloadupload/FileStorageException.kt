01c090bbe26b0b1a4b90606351439a66689f1782
yongjeong-kim <hushigine@gmail.com>
2019-02-25 14:19:44 +0900

    update

--- kotlin-download-upload/src/main/kotlin/com/kyj/kotlindownloadupload/FileStorageException.kt
+++ kotlin-download-upload/src/main/kotlin/com/kyj/kotlindownloadupload/FileStorageException.kt
@@ -3,5 +3,7 @@ package com.kyj.kotlindownloadupload
 import java.lang.RuntimeException
 
 class FileStorageException: RuntimeException {
-  constructor(message: String, cause: Throwable)
+  constructor(message: String, cause: Throwable): super(message, cause)
+  constructor(message: String): super(message)
+  constructor(cause: Throwable): super(cause)
 }
