ed83343e34e7d0bc88d169dde0de99e67d7b86e9
yongjeong-kim <hushigine@gmail.com>
2019-03-11 15:42:01 +0900

    update

--- kotlin-with-react/src/main/kotlin/com/kyj/kotlinwithreact/repository/UserRepositoryCustom.kt
+++ kotlin-with-react/src/main/kotlin/com/kyj/kotlinwithreact/repository/UserRepositoryCustom.kt
@@ -1,21 +1,14 @@
 package com.kyj.kotlinwithreact.repository
 
 import com.kyj.kotlinwithreact.domain.User
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.findAll
-import org.springframework.data.mongodb.core.findById
-import org.springframework.data.mongodb.core.findOne
-import org.springframework.data.mongodb.core.query.Criteria.where
-import org.springframework.data.mongodb.core.query.Query
-import org.springframework.data.mongodb.core.query.isEqualTo
-import org.springframework.stereotype.Repository
+import com.mongodb.client.result.DeleteResult
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-@Repository
-class UserRepositoryCustom(val template: ReactiveMongoTemplate) {
-  fun findById(id: String) = template.findById<User>(id)
-  fun findByUsername(username: String) = template.findOne<User>(Query(where("username").isEqualTo(username)))
-  fun save(user: Mono<User>) = template.save(user)
-  fun findAll() = template.findAll<User>()
-  fun deleteById(id: Int) = template.remove(Query(where("_id").isEqualTo(id)))
+interface UserRepositoryCustom {
+  fun findById(id: String): Mono<User>
+  fun findByUsername(username: String): Mono<User>
+  fun save(user: Mono<User>): Mono<User>
+  fun findAll(): Flux<User>
+  fun deleteById(id: Int): Mono<DeleteResult>
 }
--- kotlin-with-react/src/main/kotlin/com/kyj/kotlinwithreact/service/UserService.kt
+++ kotlin-with-react/src/main/kotlin/com/kyj/kotlinwithreact/service/UserService.kt
@@ -1,16 +1,16 @@
 package com.kyj.kotlinwithreact.service
 
 import com.kyj.kotlinwithreact.domain.User
-import com.kyj.kotlinwithreact.repository.UserRepositoryCustom
+import com.kyj.kotlinwithreact.repository.UserRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
 class UserService(
-  val userRepositoryCustom: UserRepositoryCustom
+  val userRepository: UserRepository
 ) {
-  fun findById(id: String) = userRepositoryCustom.findById(id)
-  fun findByUsername(username: String) = userRepositoryCustom.findByUsername(username)
-  fun findAll() = userRepositoryCustom.findAll()
-  fun createUser(user: Mono<User>) = userRepositoryCustom.save(user)
+  fun findById(id: String) = userRepository.findById(id)
+  fun findByUsername(username: String) = userRepository.findByUsername(username)
+  fun findAll() = userRepository.findAll()
+  fun createUser(user: Mono<User>) = userRepository.save(user)
 }
