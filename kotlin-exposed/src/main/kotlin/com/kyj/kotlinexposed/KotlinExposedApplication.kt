de976e3128f55c6fefc07a50fcbd054f3fd2a40d
YongJeong-Kim <hushigine@gmail.com>
2019-03-20 20:08:09 +0900

    init

---
+++ kotlin-exposed/src/main/kotlin/com/kyj/kotlinexposed/KotlinExposedApplication.kt
@@ -0,0 +1,11 @@
+package com.kyj.kotlinexposed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinExposedApplication
+
+fun main(args: Array<String>) {
+  runApplication<KotlinExposedApplication>(*args)
+}
