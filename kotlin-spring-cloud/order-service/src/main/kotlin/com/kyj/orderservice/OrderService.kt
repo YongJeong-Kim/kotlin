8f193d5cdcdf9dc3c9957532bba31c4befe58191
YongJeong-Kim <hushigine@gmail.com>
2020-04-25 22:20:27 +0900

    update

--- kotlin-spring-cloud/order-service/src/main/kotlin/com/kyj/orderservice/OrderService.kt
+++ kotlin-spring-cloud/order-service/src/main/kotlin/com/kyj/orderservice/OrderService.kt
@@ -4,19 +4,24 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono.delay
 import java.util.*
 
-@Component
+@Service
 class OrderService {
   @HystrixCommand(
     fallbackMethod = "getOrderFallback",
     commandProperties = [HystrixProperty(
       name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")])
   fun getOrder(): List<String> {
-    val random = Random().nextInt((3 - 1) + 1) + 1
-    if (random == 3)
-      Thread.sleep(12000)
+    sleep()
     return listOf("list1", "list2")
+  }
+  private fun sleep() {
+    val random = Random().nextInt((3 - 1) + 1) + 1
+    if (random == 3) {
+      Thread.sleep(32000L)
+    }
   }
 
   private fun getOrderFallback(): List<String> = listOf("getOrder fallback")
